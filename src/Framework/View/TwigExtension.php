b9ffb545fb2e8658bd296a9973a0612ae8cb8643
William Hleucka <william.hleucka@gmail.com>
2025-03-06 21:15:10 +0000

    fix: encoded ent media session

--- src/Framework/View/TwigExtension.php
+++ src/Framework/View/TwigExtension.php
@@ -3,16 +3,22 @@
 namespace Echo\Framework\View;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class TwigExtension extends AbstractExtension
 {
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new TwigFunction("csrf", [$this, "csrf"]),
             new TwigFunction("uri", [$this, "uri"]),
         ];
+    }
+
+    public function getFilters(): array
+    {
+        return [];
     }
 
     public function csrf(): string
