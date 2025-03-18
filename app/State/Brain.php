ec758a7c31d830d6195857f6fe5f35121d8115a1
William Hleucka <william.hleucka@gmail.com>
2025-03-18 19:48:47 +0000

    fix: simplify brain

--- app/State/Brain.php
+++ app/State/Brain.php
@@ -4,19 +4,9 @@ namespace App\State;
 
 class Brain
 {
-    private array $state;
-
-    public function __construct()
-    {
-        $this->state = [
-            "tracks" => container()->get(Tracks::class),
-            "playlist" => container()->get(Playlist::class),
-            "player" => container()->get(Player::class),
-        ];
-    }
-
-    public function __get(string $name): mixed
-    {
-        return $this->state[$name] ?? throw new \Exception("brain state does not exist: $name");
-    }
+    public function __construct(
+        public Tracks $tracks,
+        public Playlist $playlist,
+        public Player $player,
+    ) {}
 }
