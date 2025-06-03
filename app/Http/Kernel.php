c4687eb9b1a8f135200b6388a18cb44c051a414c
William Hleucka <william.hleucka@gmail.com>
2025-06-03 08:38:03 -0600

    fix: remove setup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,7 +9,6 @@ class Kernel extends HttpKernel
     protected array $middleware_layers = [
         \Echo\Framework\Http\Middleware\RequestID::class,
         \Echo\Framework\Http\Middleware\Sessions::class,
-        \Echo\Framework\Http\Middleware\Setup::class,
         \Echo\Framework\Http\Middleware\Auth::class,
         \Echo\Framework\Http\Middleware\Whitelist::class,
         \Echo\Framework\Http\Middleware\Blacklist::class,
