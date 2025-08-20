44d71c45ae92ceaff24e95d95b29f362e0e11e5a
William Hleucka <william.hleucka@gmail.com>
2025-08-19 21:24:36 -0600

    feat: docker

--- app/Providers/Auth/SidebarService.php
+++ app/Providers/Auth/SidebarService.php
@@ -6,43 +6,44 @@ use App\Models\User;
 
 class SidebarService
 {
-    public function getState()
-    {
-        $state = session()->get("sidebar_state");
-        if (is_null($state)) {
-            session()->set("sidebar_state", true);
-            $state = true;
-        }
-        return $state;
+  public function getState()
+  {
+    $state = session()->get("sidebar_state");
+    if (is_null($state)) {
+      session()->set("sidebar_state", true);
+      $state = true;
     }
+    return $state;
+  }
 
-    public function setState(bool $state)
-    {
-        session()->set("sidebar_state", $state);
-    }
+  public function setState(bool $state)
+  {
+    session()->set("sidebar_state", $state);
+  }
 
-    public function toggleState()
-    {
-        $state = $this->getState();
-        $this->setState(!$state);
-    }
+  public function toggleState()
+  {
+    $state = $this->getState();
+    $this->setState(!$state);
+  }
 
-    public function getLinks(array $nodes = [], array $modules = [], User $user)
-    {
-        if (empty($nodes)) {
-            $nodes = db()->fetchAll("SELECT * 
+  public function getLinks(array $nodes = [], array $modules = [], ?User $user = null)
+  {
+    if (is_null($user)) return [];
+    if (empty($nodes)) {
+      $nodes = db()->fetchAll("SELECT * 
                 FROM modules 
                 WHERE parent_id IS NULL AND enabled = 1
                 ORDER BY item_order");
-        }
-        foreach ($nodes as $node) {
-            if ($user->role === 'admin') {
-            $children = db()->fetchAll("SELECT *, CONCAT('/admin/', link) as url
+    }
+    foreach ($nodes as $node) {
+      if ($user->role === 'admin') {
+        $children = db()->fetchAll("SELECT *, CONCAT('/admin/', link) as url
                 FROM modules 
                 WHERE parent_id = ? AND enabled = 1
                 ORDER BY item_order", [$node['id']]);
-            } else {
-                $children = db()->fetchAll("SELECT *, CONCAT('/admin/', link) as url
+      } else {
+        $children = db()->fetchAll("SELECT *, CONCAT('/admin/', link) as url
                     FROM modules 
                     WHERE parent_id = ? AND 
                     enabled = 1 AND
@@ -51,15 +52,15 @@ class SidebarService
                         WHERE user_id = ? AND module_id = modules.id)
                     ORDER BY item_order
                     ", [
-                    $node['id'],
-                    $user->id
-                ]);
-            }
-            if ($children) {
-                $node['children'] = $this->getLinks($children, [], $user);
-            }
-            $modules[] = $node;
-        }
-        return $modules;
+          $node['id'],
+          $user->id
+        ]);
+      }
+      if ($children) {
+        $node['children'] = $this->getLinks($children, [], $user);
+      }
+      $modules[] = $node;
     }
+    return $modules;
+  }
 }
--- src/Framework/Database/Connection.php
+++ src/Framework/Database/Connection.php
@@ -12,137 +12,137 @@ use PDOStatement;
 
 final class Connection implements DatabaseConnection
 {
-    use Singleton;
+  use Singleton;
 
-    private bool $connected = false;
-    private ?PDO $link = null;
-    private Driver $driver;
-    private PDOStatement $stmt;
-    private array $debug = [];
+  private bool $connected = false;
+  private ?PDO $link = null;
+  private Driver $driver;
+  private PDOStatement $stmt;
+  private array $debug = [];
 
-    public function __construct(Driver $driver)
-    {
-        $this->driver = $driver;
-        $this->connect();
+  public function __construct(Driver $driver)
+  {
+    $this->driver = $driver;
+    $this->connect();
+  }
+
+  public static function getInstance(Driver $driver): Connection
+  {
+    if (self::$instance === null) {
+      self::$instance = new self($driver);
     }
+    return self::$instance;
+  }
 
-    public static function getInstance(Driver $driver): Connection
-    {
-        if (self::$instance === null) {
-            self::$instance = new self($driver);
+  public static function newInstance(Driver $driver): Connection
+  {
+    self::$instance = new self($driver);
+    return self::$instance;
+  }
+
+  public function isConnected(): bool
+  {
+    return $this->connected;
+  }
+
+  public function tryConnection(): bool
+  {
+    $this->connect();
+    return $this->connected;
+  }
+
+  private function connect(): void
+  {
+    if ($this->link === null) {
+      try {
+        $this->connected = true;
+        $this->link = new PDO(
+          $this->driver->getDsn(),
+          $this->driver->getUsername(),
+          $this->driver->getPassword(),
+          $this->driver->getOptions()
+        );
+      } catch (PDOException $e) {
+        error_log("Please refer to setup guide: https://github.com/whleucka/echo");
+
+        $debug = config("app.debug");
+        if ($debug) {
+          Flash::add("danger", "Database connection failed.");
         }
-        return self::$instance;
-    }
 
-    public static function newInstance(Driver $driver): Connection
-    {
-        self::$instance = new self($driver);
-        return self::$instance;
-    }
+        $this->connected = false;
 
-    public function isConnected(): bool
-    {
-        return $this->connected;
-    }
-
-    public function tryConnection(): bool
-    {
-        $this->connect();
-        return $this->connected;
-    }
-
-    private function connect(): void
-    {
-        if ($this->link === null) {
-            try {
-                $this->connected = true;
-                $this->link = new PDO(
-                    $this->driver->getDsn(),
-                    $this->driver->getUsername(),
-                    $this->driver->getPassword(),
-                    $this->driver->getOptions()
-                );
-            } catch (PDOException $e) {
-                error_log("Please refer to setup guide: https://github.com/whleucka/echo");
-
-                $debug = config("app.debug");
-                if ($debug) {
-                    Flash::add("danger", "Database connection failed.");
-                }
-
-                $this->connected = false;
-
-                if (preg_match('/unknown database/i', $e->getMessage())) {
-                    error_log('Unknown database. ' . $e->getMessage());
-                } else if (preg_match('/Name or service not known/', $e->getMessage())) {
-                    error_log('Unknown database host. ' . $e->getMessage());
-                } else {
-                    error_log('Unknown database error. ' . $e->getMessage());
-                }
-            }
+        if (preg_match('/unknown database/i', $e->getMessage())) {
+          error_log('Unknown database. ' . $e->getMessage());
+        } else if (preg_match('/Name or service not known/', $e->getMessage())) {
+          error_log('Unknown database host. ' . $e->getMessage());
+        } else {
+          error_log('Unknown database error. ' . $e->getMessage());
         }
+      }
     }
+  }
 
-    public function execute(string $sql, array $params = []): mixed
-    {
-        if (!$this->connected) return null;
-        $this->debug = [
-            'sql' => $sql,
-            'params' => $params,
-        ];
-        $stmt = $this->link->prepare($sql);
-        $stmt->execute($params);
-        $this->stmt = $stmt;
-        return $stmt;
-    }
+  public function execute(string $sql, array $params = []): mixed
+  {
+    if (!$this->connected) return null;
+    $this->debug = [
+      'sql' => $sql,
+      'params' => $params,
+    ];
+    $stmt = $this->link->prepare($sql);
+    $stmt->execute($params);
+    $this->stmt = $stmt;
+    return $stmt;
+  }
 
-    public function fetch(string $sql, array $params = []): array
-    {
-        return $this->execute($sql, $params)->fetch() ?: [];
-    }
+  public function fetch(string $sql, array $params = []): array
+  {
+    return $this->execute($sql, $params)?->fetch() ?: [];
+  }
 
-    public function fetchAll(string $sql, array $params = []): array
-    {
-        return $this->execute($sql, $params)->fetchAll();
-    }
+  public function fetchAll(string $sql, array $params = []): array
+  {
+    return $this->execute($sql, $params)->fetchAll();
+  }
 
-    public function lastInsertId(): string
-    {
-        return $this->link->lastInsertId();
-    }
+  public function lastInsertId(): string
+  {
+    return $this->link->lastInsertId();
+  }
 
-    public function errorInfo(): array
-    {
-        return $this->link->errorInfo();
-    }
+  public function errorInfo(): array
+  {
+    return $this->link->errorInfo();
+  }
 
-    public function errorCode(): ?string
-    {
-        return $this->link->errorCode();
-    }
+  public function errorCode(): ?string
+  {
+    return $this->link->errorCode();
+  }
 
-    public function debug(): array
-    {
-        return $this->debug;
-    }
+  public function debug(): array
+  {
+    return $this->debug;
+  }
 
-    public function beginTransaction(): bool
-    {
-        return $this->link->beginTransaction();
-    }
+  public function beginTransaction(): bool
+  {
+    return $this->link->beginTransaction();
+  }
 
-    public function commit(): bool
-    {
-        return $this->link->commit();
-    }
+  public function commit(): bool
+  {
+    return $this->link->commit();
+  }
 
-    public function rollback(): bool
-    {
-        return $this->link->rollBack();
-    }
+  public function rollback(): bool
+  {
+    return $this->link->rollBack();
+  }
 
-    public function getLink(): PDO
-    {
-        return $this->link;
-    }
+  public function getLink(): PDO
+  {
+    return $this->link;
+  }
 }
